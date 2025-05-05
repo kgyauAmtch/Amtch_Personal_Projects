2e2e0e63d0e3b7d2ec1a4611f8a99103224308aa
kgyauAmtch <kwame.ofori-gyau@amalitechtraining.org>
2025-05-05 17:18:01 +0000

    create kafka producer script

--- src/kafka_producer.py
+++ src/kafka_producer.py
@@ -1,46 +1,39 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import to_json, struct
 from kafka import KafkaProducer
-from kafka.errors import KafkaError
-import logging as log
-import os 
-
-spark=SparkSession.builder\
-       .appname('kafka_producer') \
-       .getOrCreate()
-       
+import time
+from data_generator import get_customer_message
+import logging
 
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 
-         
-spark.sparkContext.setLogLevel("WARN")
-
-''' 
-Defined the struct type of columns in dataframe in the function schema_build
-Read the each csv file from the data folder as it generates 
-Display the data in each file in the console and truncate them 
-'''
-
-# df = spark.read.option("header", "true").option("inferSchema", "true").csv("path/to/file.csv")
-# csvDF = spark.readStream.option("sep", ",").schema(sc.schema_build()).csv("/opt/spark/data/")
-csvDF = spark.readStream.option("sep", ",").schema(sc.schema_build()).csv("/data")
-# csvDF.writeStream.format("console").option("truncate", "false").start()
-
-# Convert to Kafka JSON format
-json_df = csvDF.select(to_json(struct([csvDF[col] for col in csvDF.columns])).alias("value"))
-
-# Write to Kafka in streaming mode
-json_df.writeStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("topic", "producer_to_consumer") \
-    .option("checkpointLocation", "/tmp/kafka_checkpoint") \
-    .start() \
-    .awaitTermination()
+def on_send_success(record_metadata):
+    print(f'Message delivered to {record_metadata.topic} [{record_metadata.partition}] offset {record_metadata.offset}')
 
 
-    
+def on_send_error(excp):
+    print(f'Message delivery failed: {excp}')
 
 
+def main():
+    # No need for value_serializer, messages are already encoded
+    producer = KafkaProducer(bootstrap_servers='kafka:9092')
+
+    topic = 'producer_to_consumer'
+
+    for _ in range(1, 1000):
+        message = get_customer_message()  # Already bytes
+        future = producer.send(topic, message)
+        future.add_callback(on_send_success)
+        future.add_errback(on_send_error)
+        logger.info(f'Attempting to send message: {message}')
+        time.sleep(5)
+
+    producer.flush()
+
+
+if __name__ == '__main__':
+    main()
