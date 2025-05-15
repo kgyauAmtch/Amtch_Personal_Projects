3ff46a2e1acfc539fd51f8870840f846f37f8e2f
kgyauAmtch <kwame.ofori-gyau@amalitechtraining.org>
2025-05-15 04:02:37 +0000

    create sql setup file

---
+++ db/mysql_setup.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS flightprices (
+    Airline  char(40),
+    Source   char(40),
+    Destination varchar(25),
+    BaseFare float(25),
+    Tax float(25),
+    Surcharge flaot(25),
+    Total Fare float(30)
+);
+
